083d380038484394c1508f9b4f6ecfac4ad7b16a
ambidere <xanthiandere@gmail.com>
2018-05-16 08:22:06 +0800

    Cleared current theme

--- header.php
+++ header.php
@@ -18,6 +18,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+
 
 <?php wp_head(); ?>
 </head>
